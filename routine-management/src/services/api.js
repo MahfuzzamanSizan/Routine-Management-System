e58da7a84bcbc38c86ef801422d46737f9355e36
Sizan <144158686+MahfuzzamanSizan@users.noreply.github.com>
2025-05-29 22:00:47 +0600

    package json and api.js updated

--- routine-management/src/services/api.js
+++ routine-management/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Your backend URL
+  baseURL: "https://routine-management-system-1.onrender.com", // Your backend URL
 });
 
 // API endpoints
